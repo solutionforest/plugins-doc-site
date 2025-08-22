8e5e66658fa14f7214df7ce936a325269c24f71e
carly <carly.cheung@solutionforest.net>
2025-08-22 15:56:56 +0800

    Update site title

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -11,7 +11,7 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <span className="inline-flex flex-row gap-3 items-center pb-2 [aside_&]:-ms-1.5">
-        <span className="text-xl font-bold">My Plugin</span>
+        <span className="text-xl font-bold">SolutionForest Plugin</span>
       </span>
     ),
   },
